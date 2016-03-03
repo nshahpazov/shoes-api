605e33958068fd172a4ff640c7d98c9357fd2179
nshahpazov <nshahpazov@gmail.com>
2016-03-03 15:07:52 +0200

    added more routes for del, create, etc

--- routes/orders.js
+++ routes/orders.js
@@ -2,10 +2,12 @@ var express = require('express');
 var controller = require('../controllers/orders');
 var router = express.Router();
 
-router.post('/orders/', controller.createOrder);
+router.post('/orders', controller.createOrder);
+router.get('/orders', controller.getAllOrders);
+router.delete('/orders', controller.deleteAllOrders);
 router.get('/orders/:id', controller.getOrder);
-router.post('/orders/:id/order_items', controller.createOrderItem);
-router.get('/orders/:id/order_items', controller.getOrderItems);
-router.delete('/orders/:id/order_items/:itemId', controller.createOrderItem);
+router.post('/orders/:orderId/order_items', controller.createOrderItem);
+router.get('/orders/:orderId/order_items', controller.getOrderItems);
+router.delete('/orders/:id/order_items/:itemId', controller.deleteOrderItem);
 
 module.exports = router;
