e3c0558b5fde83a9c66b49afd2145d915448af5a
nshahpazov <nshahpazov@gmail.com>
2016-03-03 16:06:22 +0200

    added routes for shoe CRUD operations

--- routes/shoes.js
+++ routes/shoes.js
@@ -3,8 +3,9 @@ var controller = require('../controllers/shoes');
 var router = express.Router();
 
 /* GET shoes listing. */
-router.get('/shoes', controller.all);
-
-// CAN HAVE OTHER API ENDPOINTS FOR CREATE, DELETE, ETC
+router.get('/shoes', controller.getAllShoeModels);
+router.get('/shoes/:id', controller.getShoeModel);
+router.post('/shoes', controller.createShoeModel);
+router.put('/shoes/:id', controller.updateShoeModel);
 
 module.exports = router;
